3c916beb37efcdcc786ffd654a350fa3ae0a6eea
Arnab Datta <arnabdt05@gmail.com>
2026-05-02 11:57:45 +0530

    priority inbox algorithm

---
+++ notification_app_fe/priorityInbox.ts
@@ -0,0 +1,27 @@
+import "dotenv/config";
+import axios from "axios";
+
+export interface Notification {
+  ID: string;
+  Type: string;
+  Message: string;
+  Timestamp: string;
+}
+
+const BASE_URL = "http://20.207.122.201/evaluation-service";
+const getToken = () => process.env.NEXT_PUBLIC_LOG_TOKEN ?? "";
+
+export async function fetchNotifications(params?: {
+  limit?: number;
+  page?: number;
+  notification_type?: string;
+}): Promise<Notification[]> {
+  const res = await axios.get(`${BASE_URL}/notifications`, {
+    headers: {
+      Authorization: `Bearer ${getToken()}`,
+    },
+    params,
+  });
+
+  return res.data.notifications;
+}
